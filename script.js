a38019ab65ff8e1d21bb53f7638bd60341e8a9a9
jkujo20 <jkujo20@yahoo.com>
2023-01-24 18:54:20 +0800

    Code adjustment

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-let removeCartItemButtons = document.getElementsByClassName("btn-danger");
+let removeCartItemButtons = document.querySelectorAll(".btn-danger");
 for (let i = 0; i < removeCartItemButtons.length; i++) {
   let button = removeCartItemButtons[i];
   button.addEventListener("click", removeCartItem);
 }
 
-let quantityInputs = document.getElementsByClassName("cart-quantity-input");
+let quantityInputs = document.querySelectorAll(".cart-quantity-input");
 for (let i = 0; i < quantityInputs.length; i++) {
   let input = quantityInputs[i];
   input.addEventListener("change", quantityChanged);
 }
 
-let addToCartButtons = document.getElementsByClassName("shop-item-button");
+let addToCartButtons = document.querySelectorAll(".shop-item-button");
 for (let i = 0; i < addToCartButtons.length; i++) {
   let button = addToCartButtons[i];
   button.addEventListener("click", addToCartClicked);
 }
 
 document
-  .getElementsByClassName("btn-purchase")[0]
+  .querySelectorAll(".btn-purchase")[0]
   .addEventListener("click", purchaseClicked);
 
 function purchaseClicked() {
   alert("Thank you for your purchase");
-  let cartItems = document.getElementsByClassName("cart-items")[0];
+  let cartItems = document.querySelectorAll(".cart-items")[0];
   while (cartItems.hasChildNodes()) {
     cartItems.removeChild(cartItems.firstChild);
   }
@@ -46,9 +46,9 @@ function quantityChanged(event) {
 function addToCartClicked(event) {
   let button = event.target;
   let shopItem = button.parentElement.parentElement;
-  let title = shopItem.getElementsByClassName("shop-item-title")[0].innerText;
-  let price = shopItem.getElementsByClassName("shop-item-price")[0].innerText;
-  let imageSrc = shopItem.getElementsByClassName("shop-item-image")[0].src;
+  let title = shopItem.querySelectorAll(".shop-item-title")[0].innerText;
+  let price = shopItem.querySelectorAll(".shop-item-price")[0].innerText;
+  let imageSrc = shopItem.querySelectorAll(".shop-item-image")[0].src;
   addItemToCart(title, price, imageSrc);
   updateCartTotal();
 }
@@ -56,8 +56,8 @@ function addToCartClicked(event) {
 function addItemToCart(title, price, imageSrc) {
   let cartRow = document.createElement("div");
   cartRow.classList.add("cart-row");
-  let cartItems = document.getElementsByClassName("cart-items")[0];
-  let cartItemNames = cartItems.getElementsByClassName("cart-item-title");
+  let cartItems = document.querySelectorAll(".cart-items")[0];
+  let cartItemNames = cartItems.querySelectorAll(".cart-item-title");
   for (let i = 0; i < cartItemNames.length; i++) {
     if (cartItemNames[i].innerText == title) {
       alert("This item is already added to the cart");
@@ -77,10 +77,10 @@ function addItemToCart(title, price, imageSrc) {
   cartRow.innerHTML = cartRowContents;
   cartItems.append(cartRow);
   cartRow
-    .getElementsByClassName("btn-danger")[0]
+    .querySelectorAll(".btn-danger")[0]
     .addEventListener("click", removeCartItem);
   cartRow
-    .getElementsByClassName("cart-quantity-input")[0]
+    .querySelectorAll(".cart-quantity-input")[0]
     .addEventListener("change", quantityChanged);
 }
 
